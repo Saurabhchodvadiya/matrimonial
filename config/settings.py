21f27db8dd22640fa05632085d855d246f48e685
Saurabh.Chodvadiya <saurabh.chodvadiya@fdmgroup.com>
2026-08-14 20:57:12 -0400

    sfs

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2fe+kmio0^+l#)k5f7mc*(*#(lmyxt9jh*s8t@(l$9(m5#g94'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
